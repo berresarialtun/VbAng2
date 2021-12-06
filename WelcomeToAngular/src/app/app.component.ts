d950c4012d15ab990d66f60ebab36ff3a6694265
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-06 16:33:57 +0300

    First day ending

--- WelcomeToAngular/src/app/app.component.ts
+++ WelcomeToAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoItem } from './models/todoItem.model';
+import { TotoItemList } from './models/todoItems.mock';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,34 @@ import { TodoItem } from './models/todoItem.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title : string = 'WelcomeToAngular';
-  intro : string = 'Bu, ilk Angular denemesi';
-  isCompleted: boolean = true; 
+  title: string = 'WelcomeToAngular';
+  intro: string = 'Bu, ilk Angular denemesi';
+  isCompleted: boolean = true;
 
-  todos: TodoItem[]=[
-    
-    new TodoItem('Angular Öğren',false),
-    new TodoItem( 'React Öğren', true ),
-    new TodoItem ('Vue Öğren', false )
-  ];
+  todos: TodoItem[] = TotoItemList;
+
+  showAll: boolean = true;
+
+  fllter(): void {
+    this.showAll = !this.showAll;
+
+    if (this.showAll) {
+      this.todos = TotoItemList;
+    }
+    else {
+      this.todos = this.todos.filter(x => !x.isDone);
+    }
+
+    let x = this.addNewItem("hebe");
+  
+  }
+
+  getFilterButtonText(): string {
+    return this.showAll ? "Filtrele (yapılanları gizle)" : "Tümünü Göster"
+  }
+  addNewItem(action: string) {
+    let item = new TodoItem(action, false);
+    this.todos.push(item);
+
+  }
 }
---
+++ WelcomeToAngular/src/app/models/todoItems.mock.ts
@@ -0,0 +1,7 @@
+import { TodoItem } from "./todoItem.model";
+
+export const TotoItemList : TodoItem[] = [
+    new TodoItem('Angular Öğren', false),
+    new TodoItem('React Öğren', true),
+    new TodoItem('Vue Öğren', false)
+  ];
