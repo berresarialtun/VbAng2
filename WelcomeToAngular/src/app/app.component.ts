78e6fe12903e8b06baec4ad759c211f902bf9c89
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-06 12:34:37 +0300

    simple binding

--- WelcomeToAngular/src/app/app.component.ts
+++ WelcomeToAngular/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WelcomeToAngular';
+  title : string = 'WelcomeToAngular';
+  intro : string = 'Bu, ilk Angular denemesi'; 
 }
