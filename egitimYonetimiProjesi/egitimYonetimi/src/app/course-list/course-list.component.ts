edbdb71f0ceff5fdcc21a9c0e8abd7527bbed10c
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-07 12:03:18 +0300

    designing components part 1

--- egitimYonetimiProjesi/egitimYonetimi/src/app/course-list/course-list.component.ts
+++ egitimYonetimiProjesi/egitimYonetimi/src/app/course-list/course-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course.model';
+import { CourseList } from '../models/mocks/course.mock';
 
 @Component({
   selector: 'app-course-list',
@@ -8,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class CourseListComponent implements OnInit {
 
   constructor() { }
+
+  courseList: Course[] = CourseList;
 
   ngOnInit(): void {
   }
