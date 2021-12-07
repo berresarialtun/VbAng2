e3529464633f436b51537bdc38d28b441aa7bab0
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-07 10:33:11 +0300

    Adding components

--- egitimYonetimiProjesi/egitimYonetimi/src/app/app.module.ts
+++ egitimYonetimiProjesi/egitimYonetimi/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { CategoryMenuComponent } from './category-menu/category-menu.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseComponent } from './course/course.component';
+import { CommentListComponent } from './comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    CategoryMenuComponent,
+    CourseListComponent,
+    CourseComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
